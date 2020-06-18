cdc5b98d857cd018570cfbeaae97205a52295881
Olof Harrysson <harrysson.olof@gmail.com>
2020-06-18 11:30:18 +0200

    Bump version: 0.0.4 → 0.1.0

--- anyfig/__init__.py
+++ anyfig/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Olof Harrysson"""
 __email__ = 'harrysson.olof@gmail.com'
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 
 import sys
 from anyfig.figutils import *
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
   test_suite='tests',
   tests_require=test_requirements,
   url='https://github.com/OlofHarrysson/anyfig',
-  version='0.0.4',
+  version='0.1.0',
   zip_safe=False,
 )
