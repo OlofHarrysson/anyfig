e14403344377f03e5d0783348c21bf3cf2e7180c
Olof Harrysson <harrysson.olof@gmail.com>
2020-06-18 12:06:56 +0200

    Only installs dataclasses for python 3.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
   readme = readme_file.read()
 
-requirements = ['fire', 'typeguard']
+requirements = ['fire', 'typeguard', "dataclasses;python_version=='3.6'"]
 
 setup_requirements = [
   'pytest-runner',
