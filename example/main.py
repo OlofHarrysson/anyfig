13dccf110f59460f8659716f51c9b5241854be47
Olof Harrysson <harrysson.olof@gmail.com>
2020-08-06 21:54:52 +0200

    Testing binder

--- example/main.py
+++
@@ -1,9 +0,0 @@
-import anyfig
-try:
-  from normalconfigs.userconfigs import UserMainConfig as ConfigClass
-except ModuleNotFoundError:
-  from normalconfigs.mainconfig import MainConfig as ConfigClass
-
-if __name__ == '__main__':
-  config = anyfig.init_config(default_config=ConfigClass)
-  print(config)
