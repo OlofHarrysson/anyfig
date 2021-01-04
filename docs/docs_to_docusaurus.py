c14cd401f7f033fe514d818cfe6fdf7c3fae1094
Olof Harrysson <harrysson.olof@gmail.com>
2021-01-04 22:21:41 +0100

    skipped auto-recursive api reference. grouped by module

--- docs/docs_to_docusaurus.py
+++ docs/docs_to_docusaurus.py
@@ -53,7 +53,10 @@ def add_docusaurus_metadata(content: str,
 def convert_to_docusaurus(soup):
   # Fix link
   for a in soup.find_all("a", {"class": "reference internal"}, href=True):
-    a["href"] = a["href"].split('.html')[0]
+    module_member = a["href"].split('.html')[0]
+    module, member = module_member.rsplit('.', 1)
+    module = module.replace('anyfig.', '')
+    a["href"] = f'{module}#{member}'
 
   # Fix table of content
   title = soup.find("h1").extract().text.replace('¶', '')
@@ -76,21 +79,22 @@ def to_md(in_html_filepath: str, out_md_filepath: str, id: str, title: str,
     f.write(content)
 
 
-def convert_modules(members, ROOT_MD_DIRECTORY):
+def convert_modules(ROOT_HTML_DIRECTORY, ROOT_MD_DIRECTORY):
+  html_modules = []
 
-  for rel_source_file, html_files in members.items():
-    # out_md_filename = f"{ROOT_MD_DIRECTORY}/api/{html_file.stem}.mdx"
+  for module_filepath in ROOT_HTML_DIRECTORY.iterdir():
+    module_name = module_filepath.name
     content = []
-    for html_file in html_files:
+    for html_file in module_filepath.glob('**/*.html'):
+      if html_file.stem == f'anyfig.{module_name}':
+        html_modules.append(html_file)
+        continue  # Summary of the module
+
       id_ = html_file.stem
       title = id_
-
-      # Create one page per file. Group them
-
       hide_title = "false"
-      # to_md(html_file, out_md_filename, id_, title, hide_title)
 
-      with open(html_file, "r") as f:
+      with open(html_file) as f:
         soup = BeautifulSoup(f.read(), "html.parser")
 
       body = get_content(soup)
@@ -98,46 +102,30 @@ def convert_modules(members, ROOT_MD_DIRECTORY):
       release = '0.2.0'  # TODO
       module = eval(id_)
       line_number = inspect.findsource(module)[1] + 1
-      # source_file = Path(inspect.getfile(module))
-      # root_source = Path(inspect.getfile(anyfig)).parent
-      # rel_source_file = source_file.relative_to(root_source)
-      url = f"https://github.com/OlofHarrysson/anyfig/blob/{release}/anyfig/{rel_source_file}#L{line_number}"
+      url = f"https://github.com/OlofHarrysson/anyfig/blob/{release}/anyfig/{module_name}#L{line_number}"
       a['href'] = url
 
-      print(html_file, rel_source_file)
+      # Fix table of content
+      title = body.find("h1").extract().text.replace('¶', '').split('.')[-1]
+      print(title)
+      body = f"\n## {title}\n{body}"
+
       content.append(str(body))
 
-    name = rel_source_file.stem
     content = '\n'.join(content)
-    content = add_docusaurus_metadata(content, name, name, hide_title)
-    out_md_filename = f"{ROOT_MD_DIRECTORY}/api/{name}.mdx"
-    print(out_md_filename)
+    content = add_docusaurus_metadata(content, module_name, module_name,
+                                      hide_title)
+    out_md_filename = f"{ROOT_MD_DIRECTORY}/api/{module_name}.mdx"
     with open(out_md_filename, "w") as f:
       f.write(content)
-
-  # with open(out_md_filename, "w") as f:
-  #   content = add_docusaurus_metadata(str(body), id_, title, hide_title)
-  #   f.write(content)
-
-  # for name, content in file_to_content.items():
-  #   print(name)
-  #   content = '\n'.join(content)
-  #   content = add_docusaurus_metadata(content, name, name, hide_title)
-  #   out_md_filename = f"{ROOT_MD_DIRECTORY}/api/{name}.mdx"
-  #   print(out_md_filename)
-  #   with open(out_md_filename, "w") as f:
-  #     f.write(content)
+  return html_modules
 
 
-def convert_root(module_dir, ROOT_MD_DIRECTORY, module_names):
+def convert_root(html_modules, ROOT_MD_DIRECTORY):
   module_content = []
-  for module_name in module_names:
-    if module_name == 'anyfig':
-      html_path = module_dir / 'anyfig.html'
-    else:
-      html_path = module_dir / f'anyfig.{module_name}.html'
+  for html_filepath in html_modules:
 
-    with open(html_path) as f:
+    with open(html_filepath) as f:
       soup = BeautifulSoup(f.read(), "html.parser")
       soup = get_content(soup)
       doc = convert_to_docusaurus(soup)
@@ -153,54 +141,16 @@ def convert_root(module_dir, ROOT_MD_DIRECTORY, module_names):
     f.write(content)
 
 
-# def convert_source(MODULE_SOURCE_DIR, ROOT_MD_DIRECTORY):
-#   for in_html_filename in MODULE_SOURCE_DIR.glob('**/*.html'):
-#     if str(in_html_filename).endswith('index.html'):
-#       continue
-
-#     out_md_filename = f"{ROOT_MD_DIRECTORY}/api/source/{in_html_filename.stem}.mdx"
-#     id_ = in_html_filename.stem
-#     title = id_
-
-#     hide_title = "true"
-#     # to_md(in_html_filename, out_md_filename, id_, title, hide_title)
-#     import html
-
-#     with open(in_html_filename) as f:
-#       text = f.read()
-#       body = html.escape(text)
-#       # text.replace('&quot;', '&-quot')
-#       # soup = BeautifulSoup(text, "html.parser")
-#       # body = get_content(soup)
-
-#       # for t in body.findAll(text=True):
-#       #   text = t.replace('\\', '&bsol;')
-#       #   t.replaceWith(html.escape(text))
-
-#       body = str(body)
-#       body = body.replace('&amp;', '&')
-
-#       # Fix syntax errors
-#       body = body.replace('class=', 'className=')
-#       # body = body.replace(
-#       #   '<col style="width: 10%"/>\n<col style="width: 90%"/>', '')
-#       # body = body.replace('"', '&quot;').replace("'", "&apos;")
-
-#     with open(out_md_filename, "w") as f:
-#       content = add_docusaurus_metadata(str(body), id_, title, hide_title)
-#       f.write(content)
-
-
 def get_module_names(MODULE_SOURCE_DIR):
   module_names = [f.stem for f in MODULE_SOURCE_DIR.glob('**/*.html')]
-  module_names.remove('index')
   module_names.remove('anyfig')  # TODO
+  module_names.remove('index')  # TODO
   return module_names
 
 
 def main():
-  ROOT_HTML_DIRECTORY = Path("./build/html")
-  MODULE_SOURCE_DIR = ROOT_HTML_DIRECTORY / '_modules'
+  ROOT_HTML_DIRECTORY = Path("./build/html/_autosummary")
+  MODULE_SOURCE_DIR = ROOT_HTML_DIRECTORY / '../_modules'
   ROOT_MD_DIRECTORY = "../website/docs"  # TODO: Change to pathlib __file__
   module_names = get_module_names(MODULE_SOURCE_DIR)
 
@@ -209,38 +159,8 @@ def main():
   Path(ROOT_MD_DIRECTORY + "api/source").mkdir(parents=True, exist_ok=True)
 
   module_dir = Path(ROOT_HTML_DIRECTORY) / '_autosummary'
-
-  html_files = list(module_dir.glob('*.html'))
-  html_files = [p for p in html_files if 'dummyfields' not in p.stem]
-
-  modules = []
-  members = defaultdict(list)
-  for html_file in html_files:
-    if 'dummyfields' in html_file.stem:
-      continue
-
-    if html_file.stem == 'anyfig':
-      modules.append(html_file)
-
-    module = eval(html_file.stem)
-    source_file = Path(inspect.getfile(module))
-    root_source = Path(inspect.getfile(anyfig)).parent
-    rel_source_file = source_file.relative_to(root_source)
-    # print(f'anyfig.{rel_source_file.stem}' == html_file.stem,
-    # rel_source_file.stem, html_file.stem)
-    if f'anyfig.{rel_source_file.stem}' == html_file.stem:
-      modules.append(html_file)
-    else:
-      members[rel_source_file].append(html_file)
-
-  # Remove modules, keep memebers # TODO
-  # html_files = [p for p in html_files if 'dummyfields' not in p.stem]
-  # print(html_files)
-  # print(module_names)
-
-  # convert_source(MODULE_SOURCE_DIR, ROOT_MD_DIRECTORY)
-  convert_modules(members, ROOT_MD_DIRECTORY)
-  convert_root(module_dir, ROOT_MD_DIRECTORY, module_names)
+  html_modules = convert_modules(ROOT_HTML_DIRECTORY, ROOT_MD_DIRECTORY)
+  convert_root(html_modules, ROOT_MD_DIRECTORY)
 
 
 if __name__ == '__main__':
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -7,11 +7,41 @@ Welcome to Anyfig's documentation!
    :caption: Contents:
 
 .. autosummary::
-  :toctree: _autosummary
+  :toctree: _autosummary/__init__
   :template: custom-module-template.rst
 
   anyfig
 
+.. autosummary::
+  :toctree: _autosummary/anyfig_setup
+  :template: custom-module-template.rst
+
+  anyfig.anyfig_setup
+
+.. autosummary::
+  :toctree: _autosummary/config_functions
+  :template: custom-module-template.rst
+
+  anyfig.config_functions
+
+.. autosummary::
+  :toctree: _autosummary/fields
+  :template: custom-module-template.rst
+
+  anyfig.fields
+
+.. autosummary::
+  :toctree: _autosummary/figutils
+  :template: custom-module-template.rst
+
+  anyfig.figutils
+
+.. autosummary::
+  :toctree: _autosummary/print_utils
+  :template: custom-module-template.rst
+
+  anyfig.print_utils
+
+
 Indices and tables
 ==================
-
