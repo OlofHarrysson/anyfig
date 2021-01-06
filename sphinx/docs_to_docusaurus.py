d2adc417b2afb620dbdbc77f9d8c2674b1d44469
Olof Harrysson <harrysson.olof@gmail.com>
2021-01-06 18:35:26 +0100

    Fix github links for classes. Cleanup

--- sphinx/docs_to_docusaurus.py
+++ sphinx/docs_to_docusaurus.py
@@ -1,14 +1,9 @@
 # Takes the output from Sphinx, clean it and send it to Docusaurus.
 
 from bs4 import BeautifulSoup
-import glob
 from pathlib import Path
-from typing import List
-import re
-import json
 import inspect
-import anyfig
-from collections import defaultdict
+import anyfig  # Needed for eval
 
 
 def main():
@@ -23,121 +18,103 @@ def main():
   convert_root(html_modules, ROOT_MD_DIRECTORY)
 
 
-def get_content(soup):
-  return soup.find("div", {"class": "body"}).find("div")
-
-
-def add_docusaurus_metadata(content: str,
-                            id: str,
-                            title: str,
-                            hide_title=False) -> str:
-  return f"---\nid: {id}\ntitle: {title}\nhide_title: {hide_title}\n---\n\n" + content
-
-
-def convert_to_docusaurus(soup):
-  # Fix link
-  for a in soup.find_all("a", {"class": "reference internal"}, href=True):
-    module_member = a["href"].split('.html')[0]
-    module, member = module_member.rsplit('.', 1)
-    module = module.replace('anyfig.', '')
-    a["href"] = f'{module}#{member}'
-
-  # Fix table of content
-  title = soup.find("h1").extract().text.replace('¶', '')
-  doc = f"\n## {title}\n{soup}"
-
-  # Fix syntax errors
-  doc = doc.replace('class=', 'className=')
-  doc = doc.replace('<col style="width: 10%"/>\n<col style="width: 90%"/>', '')
-  return doc
-
-
-def fix_document(soup):
-  soup = str(soup).replace('¶', '')
-
-  # Fix syntax errors
-  soup = soup.replace('class=', 'className=')
-  soup = soup.replace('<col style="width: 10%"/>\n<col style="width: 90%"/>',
-                      '')
-  return soup
-
-
 def convert_modules(ROOT_HTML_DIRECTORY, ROOT_MD_DIRECTORY):
-  html_modules = []
+  module_paths = []
 
   for module_filepath in ROOT_HTML_DIRECTORY.iterdir():
     module_name = module_filepath.name
     content = []
     for html_file in module_filepath.glob('**/*.html'):
       if html_file.stem == f'anyfig.{module_name}':
-        html_modules.append(html_file)
-        continue  # Summary of the module
+        module_paths.append(html_file)
+        continue
 
-      id_ = html_file.stem
-      title = id_
-      hide_title = "false"
-
-      with open(html_file) as f:
-        soup = BeautifulSoup(f.read(), "html.parser")
-
-      body = get_content(soup)
-      # print(body)
-      print(html_file.stem)
-      func_defs = body.find_all('dt')
+      soup = read_content(html_file)
+      func_defs = soup.find_all('dt')
       for func_def in func_defs:
-        # Add class
-        if not func_def.get("class"):
-          func_def['class'] = 'function-definition'
+        func_def['class'] = 'function-definition'
 
         # Code tag -> span
         for code_block in func_def.find_all('code'):
           code_block.name = 'span'
 
-      # qwe
-      source_link = body.find("a", {"class": "reference internal"})
-      release = 'master'  # TODO: Change to latest release instead?
-      module = eval(id_)
-      line_number = inspect.findsource(module)[1] + 1
-      url = f"https://github.com/OlofHarrysson/anyfig/blob/{release}/anyfig/{module_name}.py#L{line_number}"
-      source_link['href'] = url
+        # Change source link to Github
+        func_name = func_def.get('id')
+        if func_name:
+          source_link = func_def.find("a", {"class": "reference internal"})
+          release = 'master'  # TODO: Change to latest release instead?
+          func = eval(func_name)
+          line_number = inspect.findsource(func)[1] + 1
+          url = f"https://github.com/OlofHarrysson/anyfig/blob/{release}/anyfig/{module_name}.py#L{line_number}"
+          source_link['href'] = url
+          source_link['class'] = 'reference internal source-link'
 
-      # Fix table of content
-      title = body.find("h1").extract().text.replace('¶', '').split('.')[-1]
-      # print(title)
-      body = f"\n## {title}\n{body}"
-      body = str(body).replace('¶', '')
-      body = body.replace('reference internal',
-                          'reference internal source-link')
-
-      content.append(body)
+      soup = fix_document(soup)
+      content.append(soup)
 
     content = '\n'.join(content)
-    content = add_docusaurus_metadata(content, module_name, module_name,
-                                      hide_title)
     out_md_filename = f"{ROOT_MD_DIRECTORY}/api/{module_name}.mdx"
-    with open(out_md_filename, "w") as f:
-      f.write(content)
-  return html_modules
+    write_markdown(out_md_filename,
+                   content,
+                   md_id=module_name,
+                   title=module_name)
+  return module_paths
 
 
 def convert_root(html_modules, ROOT_MD_DIRECTORY):
   module_content = []
   for html_filepath in html_modules:
-
-    with open(html_filepath) as f:
-      soup = BeautifulSoup(f.read(), "html.parser")
-      soup = get_content(soup)
-      doc = convert_to_docusaurus(soup)
-
-    module_content.append(doc)
+    soup = read_content(html_filepath)
+    soup = convert_internal_link(soup)
+    soup = fix_document(soup)
+    module_content.append(soup)
 
   out_md_filename = f"{ROOT_MD_DIRECTORY}/api/anyfig.mdx"
-  with open(out_md_filename, "w") as f:
-    content = '\n'.join(module_content)
+  content = '\n'.join(module_content)
+  write_markdown(out_md_filename,
+                 content,
+                 md_id='api-reference',
+                 title='API Reference')
+
+
+def convert_internal_link(soup):
+  for a in soup.find_all("a", {"class": "reference internal"}, href=True):
+    module_member = a["href"].split('.html')[0]
+    module, member = module_member.rsplit('.', 1)
+    module = module.replace('anyfig.', '')
+    a["href"] = f'{module}#{member}'
+
+  return soup
+
+
+def read_content(html_filepath):
+  with open(html_filepath) as f:
+    soup = BeautifulSoup(f.read(), "html.parser")
+  return soup.find("div", {"class": "body"}).find("div")
+
+
+def add_docusaurus_metadata(content, md_id, title):
+  return f"---\nid: {id}\ntitle: {title}\n---\n\n" + content
+
+
+def write_markdown(md_filepath, content, md_id, title):
+  with open(md_filepath, "w") as f:
     content = add_docusaurus_metadata(content,
-                                      id='api-reference',
+                                      md_id='api-reference',
                                       title='API Reference')
     f.write(content)
+
+
+def fix_document(soup):
+  title = soup.find("h1").extract().text.replace('¶', '').split('.')[-1]
+  soup = f"\n## {title}\n{soup}"
+  soup = soup.replace('¶', '')
+
+  # Fix syntax errors
+  soup = soup.replace('class=', 'className=')
+  soup = soup.replace('<col style="width: 10%"/>\n<col style="width: 90%"/>',
+                      '')
+  return soup
 
 
 if __name__ == '__main__':
